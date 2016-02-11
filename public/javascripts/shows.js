b1bbe1ac59de868a77c9963ffee521c7311e4561
Anjie Scheuren <emailanjie@gmail.com>
2016-02-11 12:30:06 -0600

    almost have modal and event display synched

--- public/javascripts/shows.js
+++ public/javascripts/shows.js
@@ -3,7 +3,8 @@ $(function() {
 
     //show one arist, venue, date and time on the page to begin with
     function displayShow(shows) {
-        var event = shows.resultsPage.results.event[0];
+        for (var i = 0; i < shows.length; i++);
+        var event = shows.resultsPage.results.event[i];
         var artist = event.performance[0].artist.displayName;
         var venue = event.venue.displayName;
         var date = event.start.date;
@@ -89,15 +90,15 @@ $(function() {
         .done(function(data) {
             var shows = data;
             displayShow(shows);
-            var i = 1;
-            var index;
+            var i = 0;
+            var index = i++;
             //on click of x move to the next event
             $('.x').on('click', function() {
                 if (i <= 99) {
                     index = i++;
                 } else
                     return;
-                var event = shows.resultsPage.results.event[index];
+                var event = shows.resultsPage.results.event[i];
                 var artist = event.performance[0].artist.displayName;
                 var venue = event.venue.displayName;
                 var date = event.start.date;
@@ -128,22 +129,48 @@ $(function() {
 
             })
 
-            //on click of x move to the next event
+            //on click of heart, show a modal move to the next event
             $('.heart').on('click', function() {
                 var count = i;
+                console.log(index);
                 var event = shows.resultsPage.results.event[count];
                 var artist = event.performance[0].artist.displayName;
                 var venue = event.venue.displayName;
                 var date = event.start.date;
                 var time = event.start.time;
                 var songkick = event.performance[0].artist.uri;
-                // $('.modal').html('<h2 class="modalText">' + artist + ' at ' + venue + ' was added to your itinerary </h2><a href="#ok" class="ok">Awesome!</a>')
+                $('.modal').html('<h2 class="modalText"><div class="artist">' + artist + '</div><div class="venue"> at ' + venue + '</div> was added to your itinerary </h2><a href="#" class="ok">Awesome!</a>')
+
+                $('.ok').on('click', function() {
+                    count = i-1;
+                    var event = shows.resultsPage.results.event[count];
+                    var k = i;
+                    if (i <= 99) {
+                        index = i++;
+                    } else
+                        return;
+                    if (date) {
+                        date = date.replace(/(\d{4})-(\d{1,2})-(\d{1,2})/, function(match, y, m, d) {
+                            return m + '/' + d + '/' + y;
+                        });
+                    }
+
+                    $('.show').html('<a class="who link" href="' + songkick + '<h2 class="who link">' + artist + '</h2></a>');
+                    $('.show').append('<h3 class="where"> at ' + venue + '</h3>');
+                    if (time === null) {
+                        $('.show').append('<h4 class="when">' + date + ' at TBA </h4>');
+                    }
+                    if (time) {
+                        var newTime = time.split(':');
+                        var hours = Number(newTime[0]);
+                        var minutes = Number(newTime[1]);
+                        var timeValue = "" + ((hours > 12) ? hours - 12 : hours); // get hours
+                        timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes; // get minutes
+                        timeValue += (hours >= 12) ? " P.M." : " A.M."; // get AM/PM
+                        $('.show').append('<h4 class="when">' + date + ' at ' + timeValue + '</h4>');
+                    }
+                });
             })
-
-            $('.ok').on(function(artist, venue) {
-                console.log(artist, venue);
-            });
-
 
 
         })
