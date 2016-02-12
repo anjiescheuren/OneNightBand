e90396e547f2b5ac1505612e11ad3cd261c1bb7c
Anjie Scheuren <emailanjie@gmail.com>
2016-02-12 10:20:46 -0600

    creating static files

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static('public'));
+
 app.use('/', routes);
 app.use('/itinerary', itinerary);
 
