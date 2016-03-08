51bf25393b03b2a18e3c13e91ce7ce4b7f90e1d1
Anjie Scheuren <emailanjie@gmail.com>
2016-03-07 23:12:57 -0600

    venues array

--- public/javascripts/shows.js
+++ public/javascripts/shows.js
@@ -1,7 +1,7 @@
 $(function() {
   var apiRoot = 'https://api.songkick.com/api/3.0/events.json?location=geo:30.2669444,-97.7427778&per_page=100&min_date=2016-03-15&max_date=2016-03-20&apikey=PTAZie3wbuF6n5dx&jsoncallback=?';
   var eventIndex = 0;
-  var liked = [];
+  var venues = [];
 
   $.ajax({
       url: apiRoot,
@@ -15,52 +15,63 @@ $(function() {
       var shows = data.resultsPage.results.event;
 
       function formatShowObj(event, i) {
-        return {
-          artist: shows[i].performance[0].artist.displayName,
-          venue: shows[i].venue.displayName,
-          date: moment(shows[i].start.date, "YYYY-MM-DD").format("dddd, MMMM Do"),
-          time: moment(shows[i].start.time, "HH:mm:ss").format("h:mm a"),
-          songkick: shows[i].performance[0].artist.uri
+          return {
+            artist: shows[i].performance[0].artist.displayName,
+            venue: shows[i].venue.displayName,
+            date: moment(shows[i].start.date, "YYYY-MM-DD").format("dddd, MMMM Do"),
+            time: moment(shows[i].start.time, "HH:mm:ss").format("h:mm a"),
+            songkick: shows[i].performance[0].artist.uri,
+            lat: shows[i].venue.lat,
+            lng: shows[i].venue.lng
+          }
         }
-      }
 
-      var show = formatShowObj(event, eventIndex);
+      // dropMarkers(map, venues);
+
+      var venue = formatShowObj(event, eventIndex);
 
       $('.no').click(function(evt) {
         dislikeShow(); // removes current show from array
-        var show = formatShowObj(event, eventIndex);
-        displayShow(show); // displays next show in array
+        var venue = formatShowObj(event, eventIndex);
+        displayShow(venue); // displays next show in array
       });
 
       $('.yes').click(function(evt) {
         likeShow(); // shows the modal, adds the show to the DB, and removes current show from array
-        var show = formatShowObj(event, eventIndex);
-        displayShow(show);
+        var venue = formatShowObj(event, eventIndex);
+        displayShow(venue);
+        // function to filter out shows with null values
+        if (venue.name != "Unknown venue" &&
+          venue.lat != null
+        )
+          {
+            venues.push(venue);
+            console.log(venues);
+          }
       });
 
-      displayShow(show); // displays next show in array
+      displayShow(venue); // displays next show in array
 
       function dislikeShow() {
         eventIndex++;
-        var show = formatShowObj(event, eventIndex);
-        displayShow(show);
+        var venue = formatShowObj(event, eventIndex);
+        displayShow(venue);
       }
 
       function likeShow() {
-        var show = formatShowObj(event, eventIndex);
-        displayShow(show);
+        var venue = formatShowObj(event, eventIndex);
+        displayShow(venue);
         eventIndex++;
-        console.log(eventIndex);
 
-        if (show.time === "Invalid date") {
-          $('.itineraryList').append('<li class="event" id="' + eventIndex + '"><div class="showArtist">' + show.artist + '</div><div class="listItem"> at ' + show.venue + '</div><div class="listItem">' + show.date + '</div><div class="listItem"> TBA </div><a href="" class="delete">Remove</a></li>');
+        if (venue.time === "Invalid date") {
+          $('.itineraryList').append('<li class="event" id="' + eventIndex + '"><div class="showArtist">' + venue.artist + '</div><div class="listItem"> at ' + venue.venue + '</div><div class="listItem">' + venue.date + '</div><div class="listItem"> TBA </div><a href="" class="delete">Remove</a></li>');
           $('.delete').click(function(e) {
             e.preventDefault();
             $('.event#' + eventIndex).html('');
           })
         }
-        if (show.time != "Invalid date") {
-          $('.itineraryList').append('<li class="event" id="' + eventIndex + '"><div class="showArtist">' + show.artist + '</div><div class="listItem"> at ' + show.venue + '</div><div class="listItem">' + show.date + '</div><div class="listItem">' + show.time + '</div><a href="" class="delete">Remove</a></li>');
+        if (venue.time != "Invalid date") {
+          $('.itineraryList').append('<li class="event" id="' + eventIndex + '"><div class="showArtist">' + venue.artist + '</div><div class="listItem"> at ' + venue.venue + '</div><div class="listItem">' + venue.date + '</div><div class="listItem">' + venue.time + '</div><a href="" class="delete">Remove</a></li>');
           $('.delete').click(function(e) {
             e.preventDefault();
             $('.event#' + eventIndex).html('');
@@ -68,14 +79,14 @@ $(function() {
         }
       }
 
-      function displayShow(show) {
-        $('.show').html('<a class="who link" href="' + show.songkick + '<h2 class="who link">' + show.artist + '</h2></a>');
-        $('.show').append('<h3 class="where"> at ' + show.venue + '</h3>');
-        if (show.time === "Invalid date") {
-          $('.show').append('<h4 class="when">' + show.date + ' at TBA </h4>');
+      function displayShow(venue) {
+        $('.show').html('<a class="who link" href="' + venue.songkick + '<h2 class="who link">' + venue.artist + '</h2></a>');
+        $('.show').append('<h3 class="where"> at ' + venue.venue + '</h3>');
+        if (venue.time === "Invalid date") {
+          $('.show').append('<h4 class="when">' + venue.date + ' at TBA </h4>');
         }
-        if (show.time != "Invalid date") {
-          $('.show').append('<h4 class="when">' + show.date + ' at ' + show.time + '</h4>');
+        if (venue.time != "Invalid date") {
+          $('.show').append('<h4 class="when">' + venue.date + ' at ' + venue.time + '</h4>');
         }
       }
     })
