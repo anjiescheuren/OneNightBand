a4bfd150991a6b2ff2d000935b316566a9ebd0a2
Anjie Scheuren <emailanjie@gmail.com>
2016-03-17 14:44:12 -0500

    still trying to remove favicon

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var moment = require('moment');
 moment().format();
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 
 var routes = require('./routes/index');
 var itinerary = require('./routes/itinerary');
